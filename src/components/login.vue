1152177b86f90e1b15e26f07a858344d25178843
cai879174373 <879174373@qq.com>
2019-03-30 15:30:26 +0800

    登陆页面表单验证

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="login">
     <div class="box">
-      <el-form :label-position="top">
+        
+      <el-form :label-position="top" ref="loginForm"  :model='loginForm' :rules='rules'  >
         <h2>用户管理</h2>
-        <el-form-item label="用户名">
-          <el-input></el-input>
+        <el-form-item label="用户名" prop='username'>
+          <el-input v-model="loginForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
-          <el-input></el-input>
+        <el-form-item label="密码" prop='password'>
+          <el-input v-model="loginForm.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button class="btn-login" type="primary">登录</el-button>
+          <el-button  type="primary" @click="submit('loginForm')">登录</el-button>
+          <el-button  type="danger" @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -19,7 +21,43 @@
 
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data(){
+      return {
+
+        //   表单验证功能
+          loginForm:{
+              username:'',
+              password:''
+          },
+          rules:{
+               username:[
+                   {required:true,message:'用户名不能为空',trigger:'blur'},
+                   {min:3,max:12,message:'长度在3-12个字符',trigger:'blur'},
+               ],
+               password:[
+                   {required:true,message:'密码不能为空',trigger:'blur'},
+                   {min:4,max:16,message:'长度在4-16个字符',trigger:'blur'},
+               ],
+
+          }
+      }
+  },
+  methods: {
+      submit(formName){
+          this.$refs[formName].validate(valid=>{
+              if(valid){
+                //   成功提交数据
+              }else{
+                  this.$message.error('提交数据有误,请重新提交')
+                  return false
+              }
+          })
+      },
+      resetForm(formName){
+           this.$refs[formName].resetFields();
+      }
+  },
 };
 </script>
 
